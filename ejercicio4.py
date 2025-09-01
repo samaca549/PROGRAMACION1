b8c210222fb80d523883d5492a5b6564c4f496e7
samaca549 <msamacar@unal.edu.co>
2025-09-01 18:31:29 -0500

    sigo con mas elif

--- ejercicio4.py
+++ ejercicio4.py
@@ -3,3 +3,5 @@ primernumero=float(input("digite el primer numero para comparacion:"))
 segundonumero=float(input("digite el primer numero para comparacion:"))
 if(primernumero>segundonumero):
     print("el primer numero es mayor.")
+elif(primernumero<segundonumero):
+    print("el segundo numero es mayor")
