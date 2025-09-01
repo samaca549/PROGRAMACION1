51ca2a8dd4624cec4d9481940310321c684b4926
samaca549 <msamacar@unal.edu.co>
2025-09-01 18:29:32 -0500

    le agrego un print

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,3 +3,4 @@ print("vamos a calcular el area de un rectangulo proporcione los datos.")
 altura=float(input("digite la medida de la altura del rectangulo:"))
 base=float(input("digite la medida de la base del rectangulo:"))
 area=altura*base
+print(f"el area del ractangulo de las magnitudes dadas es {area}")
