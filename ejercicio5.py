c0034f78361b49232f1fd065e233cf3b1ff04eea
samaca549 <msamacar@unal.edu.co>
2025-09-01 18:35:54 -0500

    realizo las operaciones

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,3 +2,5 @@
 a=float(input("digite el coeficiente a:"))
 b=float(input("digite el coeficiente b:"))
 c=float(input("digite el coeficiente c:"))
+operacion=((-b)+(b**2-4*a*c)**0.5)/(2*a)
+operacion1=((-b)-(b**2-4*a*c)**0.5)/(2*a)
